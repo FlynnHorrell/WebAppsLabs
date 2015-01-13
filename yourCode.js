023339b4ff5460eed3e161fdc8708076d692a7db
Flynn Horrell <horrellf16@hanover.edu>
2015-01-13 14:34:08 -0500

    BinarySearch Completed

--- yourCode.js
+++ yourCode.js
@@ -8,15 +8,29 @@
  */
 var binarySearch = function binarySearch(arr, val) {
    var lo, hi, mid;
-
-   // You may need to add things here
-
-   while (false) {     // You should change this with a proper condition
-      // You will need to add things here
+   lo = 0;
+   hi = arr.length-1;
+   mid = Math.floor(hi/2);
+   
+   if(arr.length===1){
+   	return arr[mid]===val;
    }
-
    // You may need to add things here
-
+   while (lo<=hi) {     // You should change this with a proper condition
+      // You will need to add things here
+      if(arr[mid]===val){
+        return true;
+      }
+      if(val>arr[mid]){
+      	lo = mid+1;
+      }
+      if(val<arr[mid]){
+      	hi = mid-1;
+      }
+      mid = Math.floor((lo+hi)/2);
+   }
+   return false;
+   // You may need to add things here
 };
 
 /*
