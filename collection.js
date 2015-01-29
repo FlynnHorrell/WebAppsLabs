1d71a9cc3b8db8a0ba2fab26917841d30968132f
Luke Lentz <lentzl16@hanover.edu>
2015-01-29 18:26:16 -0500

    proto

--- collection.js
+++ collection.js
@@ -11,36 +11,86 @@ Task = require("./task");
 /*
  *       Constructors
  */
+function addOneTask(task){
+	"use strict";
+if (!has(task)){
+   this.values.push(task);
+}
+}
 
-function makeNewCollection(arr) {
-	collection = Object.create(proto);
+
+function help(a){
+	"use strict";
+   return this.values.indexOf(a);
+}
+
+
+function makeNewCollection(arr){
+	"use strict";
+	var collection = Object.create(proto);
 	this.defineProperty(collection, "values", {
-		values: [],
+		value: [],
 		writable: false
-	}
-	if Array.isArray(arr){
-		forEach(add);
+	});
+	if (Array.isArray(arr)){
+		values.forEach(add);
 	}
 	Object.preventExtensions(collection);
 
-};
+}
 
 
 /*
  *       Prototype / Instance methods
  */
 
-proto={
+proto = {
    length: function length(){
-   	"use strict";
-   	return collection.values.length;
-   }
+   "use strict";
+   return this.values.length;
+   },
    isEmpty: function isEmpty(){
-   	"use strict";
-   	return collection.values.isEmpty();
+   "use strict";
+   return this.values.isEmpty();
+   },
+/*   get: function get(arg){
+   "use strict";
+   if (typeof arg === "function"){
+   return arg(this.values);
+   }else if (typeof arg === "number"){
+   return this.values[ help(arg) ]
    }
-   
-}
+    }*/
+  has: function has(arg){
+  "use strict";
+  return help(arg) !== -1;
+   },
+  add: function add(task){              //  adds multiple tasks from an array
+     "use strict";
+  tasks.forEach(addOneTask);
+  return this;
+  },
+  new: function new(){
+  "use strict";
+  this.values.push(Task.new());
+  },
+  remove: function remove(arg){
+  "use strict";
+  if (Array.isArray(arg)){
+    for (var i = 0;i < arg.length-1;i+=1){
+        this.values.splice(help(arg), 1);
+    }
+    return this;
+  }
+  },
+  filter: function filter(arg){
+  },
+  forEach: function forEach(f){   //f is a function
+  values.forEach(f);
+  return this;
+  }
+};
+
 
 
 
